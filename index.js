6f4273b448e856b0c4d28d05e6ee1a690406d157
shital chatarkar <shitalchatarkar21@gmail.com>
2025-06-22 15:29:11 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
+require(".env").config()
 const app = express()
 app.use(express.json())//req.body parsal
+app.use(express.static("dist"))//req.body parsal
 
 app.use("/api/v1/blog", require("./routes/blog.router"))
-mongoose.connect("mongodb://localhost:27017/skillhub")
+mongoose.connect(process.env.MONGODB_URL)
 mongoose.connection.once("open", () => {
     console.log("db connection success")
 
-    app.listen(5000, console.log("server success"))
+    app.listen(process.env.PORT, console.log("server success"))
 })
