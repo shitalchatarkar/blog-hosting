359f9e99b74ac7053261e7431016d66ebbc3f4dd
shital chatarkar <shitalchatarkar21@gmail.com>
2025-06-22 16:25:29 +0530

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 require("dotenv").config()
 const app = express()
 app.use(express.json())//req.body parsal
 app.use(express.static("dist"))//req.body parsal
+app.use(cors({ origin: "http://localhost:5173" }))
 
 app.use("/api/v1/blog", require("./routes/blog.router"))
 mongoose.connect(process.env.MONGODB_URL)
